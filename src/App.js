70ef658873d9d4c678c4986235fb63ab3a75bd56
Chris Pua <chris.pua@mail.utoronto.ca>
2021-01-01 16:06:10 -0500

    Created new personal webpage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+import { Link } from 'react-scroll';
+
 import './App.css';
+import picture from './Profile.jpg';
+import skills from './Skills.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main>
+      <div className="Home">
+        <h1 className="Name">
+          Chris Pua
+        </h1>
+        <p className="Description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce erat leo, elementum ut nibh quis. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+        <Link activeClass="active" to="About" spy={true} smooth={true} duration={1000}>
+          <Button inverted color="red" size="huge">
+          Who am I?
+          </Button>
+        </Link>
+      </div>
+
+      <div id="About">
+        <div className="About-Profile">
+          <img src={picture} className="About-Picture"/>
+
+          <div className="About-Description">
+            <h1 className="About-Title">
+              About Me
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce erat leo, elementum ut nibh quis. 
+            </p>
+            <div className="About-Buttons">
+              <Button inverted color="blue" size="huge">
+                Resume
+              </Button>
+              <Button inverted color="blue" size="huge">
+                Projects
+              </Button>  
+            </div>
+          </div>
+        </div>
+        
+        <div className="About-Skills-Section">
+          <img src={skills} className="About-Skills"/>
+        </div>
+        <Link activeClass="active" to="Contact" spy={true} smooth={true} duration={2500}>
+          <Button inverted color="blue" size="huge">
+            Contact Information
+          </Button>  
+        </Link>
+        
+      </div>
+
+      <div id="Contact">
+        <div className="Contact-Description">
+          <p>
+            Let's talk.
+          </p>
+          <p className="Contact-Email">
+            <u>
+              chris.pua@mail.utoronto.ca
+            </u>
+          </p>
+          <div className="Contact-Links">
+            <a href="https://www.linkedin.com/in/christopher-pua/">
+              <u>
+                LinkedIn
+              </u>
+            </a>
+            <a href="https://github.com/PuaChris">
+              <u>
+                GitHub
+              </u>
+            </a>
+            <a href="https://medium.com/@chrisatpua">
+              <u>
+                Medium
+              </u>
+            </a>
+          </div>  
+        </div>
+        
+      </div>
+    </main>
   );
 }
 
