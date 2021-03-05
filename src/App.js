2fa6b80c221dca536e6acc7ef765fdb2732ad07f
Chris Pua <chris.pua@mail.utoronto.ca>
2021-03-05 10:16:04 -0500

    Updated resume

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
             </p>
             <div className="About-Buttons">
               <Button inverted color="blue" size="huge">
-                <a href="https://drive.google.com/file/d/1pUJDXnCMEFEVNiI-Ka5MlFp7GJJix4PS/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1Ab8YqdaLarf8p5UMs3SCNxWW9vuamhZy/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                   Resume
                 </a>
               </Button>
